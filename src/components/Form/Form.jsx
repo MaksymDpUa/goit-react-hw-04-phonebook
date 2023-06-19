944072537348e6c45b7f78f7b084ab47f9d04b4a
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-06-19 16:31:49 +0200

    finished task

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,73 +1,83 @@
+import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
-import { Component } from 'react';
+import { useState } from 'react';
 
 import css from './Form.module.css';
 
-export class Form extends Component {
-  state = {
-    name: '',
-    number: '',
+export const Form = ({ isInList, addContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const inputNameId = nanoid();
+  const inputTelId = nanoid();
+
+  const handleInputChange = e => {
+    switch (e.target.name) {
+      case 'name':
+        setName(e.target.value);
+        break;
+      case 'number':
+        setNumber(e.target.value);
+        break;
+      default:
+        console.log('Something wrong');
+    }
   };
 
-  inputNameId = nanoid();
-  inputTelId = nanoid();
-
-  handleInputChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
+    
     e.preventDefault();
 
-    const { name, number } = this.state;
-    
-    if (this.props.isInList(name)) {
+    if (isInList(name)) {
       alert(`${name} is already in your contacts`);
       return;
     }
 
     const id = nanoid();
-    this.props.addContact([{ name, id, number: Number(number) }]);
-
-    this.setState({ name: '', number: '' });
+    addContact([{ name, id, number: Number(number) }]);
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <form className={css.form} onSubmit={this.handleSubmit}>
-        <label htmlFor={this.inputNameId} className={css.inputLable}>
-          Name
-        </label>
-        <input
-          id={this.inputNameId}
-          className={css.inputField}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          onChange={this.handleInputChange}
-          value={this.state.name}
-        />
-        <label htmlFor={this.inputTelId} className={css.inputLable}>
-          Number
-        </label>
-        <input
-          id={this.inputTelId}
-          type="tel"
-          className={css.inputField}
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          value={this.state.number}
-          onChange={this.handleInputChange}
-        />
-        <button type="submit" className={css.submitBtn}>
-          Add contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <label htmlFor={inputNameId} className={css.inputLable}>
+        Name
+      </label>
+      <input
+        id={inputNameId}
+        className={css.inputField}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        onChange={handleInputChange}
+        value={name}
+      />
+      <label htmlFor={inputTelId} className={css.inputLable}>
+        Number
+      </label>
+      <input
+        id={inputTelId}
+        type="tel"
+        className={css.inputField}
+        name="number"
+        pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        value={number}
+        onChange={handleInputChange}
+      />
+      <button type="submit" className={css.submitBtn}>
+        Add contact
+      </button>
+    </form>
+  );
+};
+
+
+Form.propTypes = {
+  addContact: PropTypes.func,
+  isInList: PropTypes.func,
 }
